d193ae77919436afa05817e8527c7c30668f19b3
Danny Smith <drs1@williams.edu>
2014-11-20 12:39:39 -0500

    registers in memory

--- p1/phase1.s
+++ p1/phase1.s
@@ -1,11 +1,43 @@
-;;; phase 1 of the final project
-;;; (c) 2014 d.r.smith
-	
-	.requ	wpc, r8
+;;; emulator for warm - phase 2
+;;; (c) d.r.smith modsoussi bijan
+
+	.requ	wpc, r15
 	lea	WARM,r0
 	trap	$SysOverlay
 	
-	mov	$0,r10
+	mov	$0,wpc
+	mov	$0,(rbp)
+	mov	$0,1(rbp)
+	mov	$0,2(rbp)
+	mov	$0,3(rbp)
+	mov	$0,4(rbp)
+	mov	$0,5(rbp)
+	mov	$0,6(rbp)
+	mov	$0,7(rbp)
+	mov	$0,8(rbp)
+	mov	$0,9(rbp)
+	mov	$0,10(rbp)
+	mov	$0,11(rbp)
+	mov	$0,12(rbp)
+	mov	$0,13(rbp)
+	mov	$0,14(rbp)
+	mov	$0,15(rbp)
+
+;;; lets add
+;;; decode the instruction
+
+	mov	$1, r0
+	shr	r0, $26
+	and	r0, WARM(r0)
+	be	arith
+
+arith:	
+
+
+
+
+	
+	
 loop:	mov	WARM(r10),r0
 	cmp	$0x06800000,r0
 	je	found
--- p1/warm.s
+++ p1/warm.s
@@ -1,6 +1,5 @@
-;;; example program, reads number writes it out
 	swi	#SysGetNum
 	swi	#SysPutNum
-	mov	r0, #10
-	swi	#SysPutChar
+	mov	r0,#10
+	swi 	#SysPutChar
 	swi	#SysHalt
