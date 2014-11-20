c4acf027e5d985ff9e164b193da9493c0acb0900
Danny Smith <drs1@williams.edu>
2014-11-20 11:29:07 -0500

    gets rid of stack

--- wave2.s
+++ wave2.s
@@ -7,25 +7,6 @@
 	mov	$1000,r0
 	trap	$SysOverlay
 
-	.equ	a1, -4
-	.equ	a2, -8
-	.equ	a3, -12
-	.equ	a4, -16
-	.equ	v1, -20
-	.equ	v2, -24
-	.equ	v3, -28
-	.equ	v4, -32
-	.equ	v5, -36
-	.equ	v6, -40
-	.equ	v7, -44
-	.equ	v8, -48
-	.equ	v9, -52
-	
-
-	push 	rbp
-	mov	rsp, rbp
-	sub	rsp,$64
-	
 	mov	1000(wpc), r0
 	mov	r0, cur_instr
 	sar	r0, $26
