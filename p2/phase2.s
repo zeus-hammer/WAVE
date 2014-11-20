e6df0093dd8a5a1c3649615f90f9b1f25f37cd39
Danny Smith <drs1@williams.edu>
2014-11-20 12:40:38 -0500

    registers now in memory

--- p2/phase2.s
+++ p2/phase2.s
@@ -1,28 +1,24 @@
 ;;; emulator for warm - phase 2
 ;;; (c) d.r.smith modsoussi bijan
 
- 	.requ	wpc,r11
-	.requ	cur_inst, r1
-	mov	$0,wpc
-	mov	$1000,r0
+	.requ	wpc, r15
+	lea	WARM,r0
 	trap	$SysOverlay
-
-	mov	1000(wpc), r0
-	mov	r0, cur_instr
-	sar	r0, $26
-	be	arith
-
-arith:	mov	$15, r2
-	sal	r2, $22
-	cmp	r2,r1
-	be	add
-
-add:	mov	cur_instr, r0
-	sar	r0, $14
-	mov	r0, r3		;src register
-	mov	cur_instr, r0
-	sar 	r0, $18
-	mov	r0, r4		;dest register
 	
-	
+	mov	$0,r10
+	mov	$1,(r10)
+loop:	mov	WARM(r10),r0
+	cmp	$0x06800000,r0
+	je	found
+	add	$1,r10
+	jmp	loop
+
+found:	mov	r10, r0
+	trap	$SysPutNum
+	mov	$'\n, r0
+	trap	$SysPutChar
+	trap	$SysHalt
+WARM:	 
+
+
 	
