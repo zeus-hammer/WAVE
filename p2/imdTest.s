48f6001468cd7f91ce9ba257f203f001c131c85b
Danny Smith <drs1@williams.edu>
2014-11-24 18:12:53 -0500

    finish immediate and add

--- p2/imdTest.s
+++ p2/imdTest.s
@@ -1,8 +1,4 @@
 ;;; warm test for immediate mode
 ;;; (c) drsmith
-
-
-	
 main:	
-	add	r0, r0, #3
 	add	r1, r0, #2
--- p2/phase2.s
+++ p2/phase2.s
@@ -6,16 +6,18 @@
 	.requ	op, r11
 	.requ	src, r10
 	.requ	dst, r9
+	.requ 	rhs, r8
 	
 	.requ	work0, r1
 	.requ	work1, r2
 	.requ	work2, r3
 	.requ	work3, r4
-	
 		
  	.equ	maskT, 0xc000000 	;27 and 26th bit
 	.equ	maskA, 0x7800		;1 in 14,13,12th bits
-	.equ	mask4, 0xE
+	.equ	mask4, 0xf
+	.equ	maskValue, 0x1ff
+	.equ	maskExp, 0x1f00
 	
 	lea	WARM,r0
 	lea	REGS,reg
@@ -28,41 +30,45 @@ fetch:	mov	WARM(wpc),ci
 	and	ci, work0
 	shr	$31, work0	;work 0 holds the type
  	mov	TYPE(work0), rip
+;;; INSTRUCTION TYPES
 arith:
 	mov 	$maskA, work0
 	and 	ci,work0
 	shr	$14, work0	;work 0 holds the addressing mode
 	mov	ADDR(work0), rip
+;;; ADDRESSING MODES
 imd:
+	mov	ci, src
+	shr	$15, src
+	and	$mask4, src
+	mov	ci, dst
+	shr	$19, dst
+	and	$mask4, dst
 	mov	ci, work0
-	shr	
-rim:
-
-rsr:
-
-rpm:	
-
-	
+	and	$maskExp, work0	;exponent
+	shr	$9, work0
+	mov	ci, rhs
+	and	$maskValue, rhs	;value
+	shl	work0, rhs
 	mov 	ci,op
 	shl	$4,op
 	shr	$27,op
 	mov	INSTR(op), rip
 	mov	ci,work0
+rim:
 
+rsr:
 
-
-ADDR:
-	.data 	imd, imd, imd, imd, rim, rsr, rpm
+rpm:
 
 ls:
 
 branch:	
 
-	
-
-
+;;; INSTRUCTIONS
 add:
-
+	add	REGS(src), rhs
+	mov	rhs, REGS(dst)
 adc:
 	
 sub:
@@ -117,5 +123,7 @@ INSTR:
 	.data 	add,adc,sub,cmp,eor,orr,and,tst,mul,mla,div,mov,mvn,swi,ldm,stm,ldr,str,ldu,stu,adr,0,0,0,bf,bb,blf,blb
 TYPE:
 	.data	arith, arith, ls, branch
+ADDR:
+	.data 	imd, imd, imd, imd, rim, rsr, rpm
 
 WARM:	 
