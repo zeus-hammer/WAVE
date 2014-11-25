0a1c24e837ef12fb3f4e0de8a2f466f6096bff0b
Danny Smith <drs1@williams.edu>
2014-11-25 06:22:08 -0500

    added second mul test, passing all mul tests

--- p2/mulTest2.s
+++ p2/mulTest2.s
@@ -4,6 +4,6 @@
 
 	add	r0, r0, #1	;1 should be in r0
 	mul	r1, r0, #3	;r1 should be 3
-	add	r2, r1, r1, lsl #1 ;9 should be in r2
-	add	r3, r2, r2, lsr r0 ;13 should be in r3
+	mul	r2, r1, r1, lsl #1 ;r2 should be 18
+	mul	r3, r2, r2, lsr r0 ;r3 should be 162
 	
